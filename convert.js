de01333adf1e7142a01c0d603c30a7a01daa42f0
Emily Nguyen <emnguyen@Emilys-MBP.home>
2016-12-14 14:40:06 -0800

    Implemented reverse, base 1, and n check; started error messages

--- convert.js
+++ convert.js
@@ -13,6 +13,25 @@ function getVal2() {
 };
 
 /*
+ * Returns class of the current active unit
+ */
+function getUnitClass(whichUnit) {
+    if (whichUnit == 1)
+        var unit = "#unit1";
+    else
+        var unit = "#unit2";
+    
+    if ($(unit +' .active-unit').hasClass('dec') == true)
+        return "dec";
+    if ($(unit +' .active-unit').hasClass('bin') == true)
+        return "bin";
+    if ($(unit +' .active-unit').hasClass('hex') == true)
+        return "hex";
+    else
+        return "baseN";
+};
+
+/*
  * Update which unit is active upon click; add/remove active-unit class
  */
 function switchUnits(newUnit, whichUnit) {
@@ -31,6 +50,33 @@ function switchUnits(newUnit, whichUnit) {
 };
 
 /*
+ * Reverse the selected units
+ */
+function reverseUnits() {
+  // Get unit 1 and unit 2 values
+  var unit1 = getUnitClass(1);
+    var unit2 = getUnitClass(2);
+    
+    // Switch
+    $('#unit1 .active-unit').removeClass('active-unit');
+  $('#unit1 .' + unit2).addClass('active-unit');
+    
+     $('#unit2 .active-unit').removeClass('active-unit');
+  $('#unit2 .' + unit1).addClass('active-unit');
+    
+    // Switch n1 and n2
+    var n1 = $("[name='n1']").val();
+    $("[name='n1']").val($("[name='n2']").val());
+    $("[name='n2']").val(n1);
+    
+    // Switch inputs
+    var val1 = $("[name='val1']").val();
+    $("[name='val1']").val($("[name='val2']").val());
+    $("[name='val2']").val(val1);
+    
+};
+
+/*
  * Get remainder when base is > 10
  */
 function getHexRemainder(remainder) {
@@ -44,9 +90,10 @@ function getHexRemainder(remainder) {
 function decToBaseN(dec, base) {
     var num = [];
     
-    if (dec == 0)
+    if (dec === 0 && base != 1)
         return 0;
     
+    if (base != 1) {
     while (dec) { 
         var remainder = dec % base;
         if (remainder > 9)
@@ -56,14 +103,30 @@ function decToBaseN(dec, base) {
     }
     if (base == 16)
         num.unshift("0x");
-    return num.join("");
+   
+    }
+    else {
+        if (dec == 0) {
+            return "NaN";
+        }
+        while (dec) {
+            num.unshift("1");
+            dec--;
+            if (num.length > 64)
+                return "result too large";
+        }
+    }
+     return num.join("");
 };
 
 /*
  * Converts a number in a given base to decimal
  */
 function baseNtoDec(num, base) {
-    return parseInt(num, base);  
+    if (base != 1) {
+        return parseInt(num, base); 
+    }
+    return num.length;
 };
 
 /*
@@ -103,6 +166,26 @@ function convert(num) {
 }
 
 /*
+ * Check that input for n1/n2 is valid
+ */
+function checkN(input, whichN) {
+    var n = $(input).val();
+    
+    // Return if valid input
+    if (n == parseInt(n, 10) && n && n < 37 && n > 0)
+        return;
+    
+    // Return if value is still n
+    if (n == "n")
+        return;
+    
+    // Reset to n and show error message otherwise
+     $(input).val("n");
+     $('.message' + whichN).text("Invalid value for n! n must be an integer between 0 and 37.");
+    
+};
+
+/*
  * MAIN FUNCTION
  */
 var main = function() {
@@ -121,12 +204,27 @@ var main = function() {
         $("[name='val2']").val(num);
     });
     
+    // REVERSE BUTTON
+    $('.reverse').click(function() {
+        reverseUnits();
+    });
+    
     // CLEAR BUTTON
     $('.clear').click(function () {
         $("[name='val1']").val('');
         $("[name='val2']").val('');
        
     });
+    
+    // CHECK INPUT FOR N
+    $("[name='n1']").on('blur',function () {
+       checkN(this, 1);
+    });
+    $("[name='n2']").on('blur',function () {
+       checkN(this, 2);
+    });
+    
+   
 };
 
 $(document).ready(main);
